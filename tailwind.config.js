346d7dfede15e2eafa0db6ef03d9522f79d9aeb7
kzmkts <94277866+kzmkts@users.noreply.github.com>
2022-01-05 20:03:27 +0900

    update:tailwind3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  mode: 'jit',
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     container: {
       center: true,
